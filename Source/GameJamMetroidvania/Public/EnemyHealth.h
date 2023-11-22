526b17ea256e435e580d37f976b7527d260f1c57
MrAzarel <arseniy.mazurenko.03@gmail.com>
2023-11-22 13:18:22 +0300

    Start enemy health

---
+++ Source/GameJamMetroidvania/Public/EnemyHealth.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "EnemyHealth.generated.h"
+
+UCLASS()
+class GAMEJAMMETROIDVANIA_API AEnemyHealth : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	EnemyHealth();
+
+	float GetHealth() const { return Health; }
+protected:
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	float MaxHealth = 100.0f;
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	float Health = 0.0f;
+
+};
