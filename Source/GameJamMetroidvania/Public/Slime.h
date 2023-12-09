c1291c5f2163766a514134164866347eaa1e8297
Arsenii <arseniy.mazurenko.03@gmail.com>
2023-12-09 19:38:22 +0300

    Slime Added

---
+++ Source/GameJamMetroidvania/Public/Slime.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnemyBase.h"
+#include "Slime.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEJAMMETROIDVANIA_API ASlime : public AEnemyBase
+{
+	GENERATED_BODY()
+	
+};
